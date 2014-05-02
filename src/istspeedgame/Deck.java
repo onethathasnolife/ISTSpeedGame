0a435532da83b0cd23249a40e8efa6d6e8d6e728
Steven Yencho <onethathasnolife@gmail.com>
2014-05-01 22:35:15 -0400

    Splitting Logic Added. Should be right, untested.

--- src/istspeedgame/Deck.java
+++ src/istspeedgame/Deck.java
@@ -7,8 +7,8 @@ public class Deck {
 	String [] suits  = {"s","c","h","d"};
 	String [] values = {"2","3","4","5","6","7","8","9","10","j","q","k","a"};
 	Card [] cards = new Card[52];
-	Card [] tabledeckLeft= new Card[5];
-	Card [] tabledeckRight = new Card[5];
+	Card [] tableDeckLeft= new Card[5];
+	Card [] tableDeckRight = new Card[5];
 	Card [] playerDeckA = new Card[21];
 	Card [] playerDeckB = new Card[21];
 	
@@ -31,8 +31,32 @@ public class Deck {
 	}
 	
 	public void Split() {
-		
-	}
+		int j = 1; //Deck A or Deck B
+		int k = 0; //right
+		int l = 0; //playerDeckA
+		int m = 0; //playerDeckB
+		for(int i = 0;i<52;i++){
+			if(i <5){
+				tableDeckLeft[i] = deck.get(i);
+			}// i < 5
+			if(i >=5 && i < 10){
+				tableDeckRight[k] = deck.get(i);
+				k++;
+			}// i >= 5 && i < 10
+			if(i >= 10){
+				if(j ==1){
+					playerDeckA[l] = deck.get(i);
+					l++;
+					j--;
+				}//A
+				else{
+					playerDeckA[m] = deck.get(i);
+					m++;
+					j++;
+				}//B
+			}//i >= 10
+		}// for : i
+	}// Split
 	
 	public Card getCard(int index) {
 		return deck.get(index);
